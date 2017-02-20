35137fbd3fdb2679562b1a48e9c9eb35209c5a46
Lucas Bicudo <Lucas.Bicudo@techsolcom.local>
2017-02-20 11:19:55 -0500

    Lecture 43 to 50

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'Book 01' },
-    { title: 'Book 02' },
-    { title: 'Book 03' },
-    { title: 'Book 04' }
-  ]
+    { title: 'Book 01', pages: 101 },
+    { title: 'Book 02', pages: 102 },
+    { title: 'Book 03', pages: 103 },
+    { title: 'Book 04', pages: 104 }
+  ];
 }
